2cb3361871b0ed54a05223689e3cf072652dd83c
kat <k.lukens@gmail.com>
2026-09-03 19:16:08 -0600

    first class functions woo

--- cozy_field.lua
+++ cozy_field.lua
@@ -6,7 +6,7 @@ function new_field(x,y)
       field[i][j]=new_square()
     end
   end
-  printh("field created")
+  log("field created")
   return field
 end
 
@@ -18,10 +18,11 @@ function new_square()
       square.spr_soil=2
     end
     square.start_time=time()
+    square.spr_plant=0
     return square
 end
 
-function update_field(default_wet_duration_s)
+function update_field(wet_duration_s)
   for i=1, field_size_x do
     for j=1, field_size_y do
       local square=field[i][j]
@@ -32,19 +33,55 @@ function update_field(default_wet_duration_s)
   end
 end
 
-function water_square()
+-- function water_square()
+--   for i=1, field_size_x do
+--     for j=1, field_size_y do
+--       local square=field[i][j]
+--       local sx=i*(8+pad_x)
+--       local sy=j*(8+pad_y)
+--       local dist=max(abs(player.x-sx), abs(player.y-sy))
+--       if dist<8 and not is_wet(square) and btn(5) then
+--         square.spr_soil=2
+--         square.start_time=time()
+--         log("[%][%] watered", i, j)
+--       end
+--     end
+--   end
+-- end
+
+is_being_watered = function(square)
+  if not is_wet(square) and btn(5) then
+    return true
+  else
+    return false
+  end
+end
+
+water_square = function(square)
+  square.spr_soil = 2
+  square.start_time = time()
+end
+
+function water_squares()
+  field_interacter("watering", is_being_watered, water_square)
+end
+
+function plant_square()
   for i=1, field_size_x do
     for j=1, field_size_y do
       local square=field[i][j]
       local sx=i*(8+pad_x)
       local sy=j*(8+pad_y)
       local dist=max(abs(player.x-sx), abs(player.y-sy))
-      if dist<8 and not is_wet(square) and btn(5) then
-        square.spr_soil=2
-        square.start_time=time()
+      if dist<8 and is_empty(square) and btn(4) then
+        square.spr_plant=3
+        log("[%][%] planted", i, j)
       end
     end
   end
+end
+
+function grow_plant()
 end
 
 function draw_field(pad_x, pad_y)
--- cozy_helpers.lua
+++ cozy_helpers.lua
@@ -6,3 +6,37 @@ function is_wet(square)
   return square.spr_soil==2
 end
 
+function is_empty(square)
+  return square.spr_plant==0
+end
+
+function log(fmt, ...)
+  local args={...}
+  local n=0
+  local out=""
+  for i=1, #fmt do
+    local c=sub(fmt, i, i)
+    if c=="%" then
+      n=n+1
+      out=out..tostr(args[n])
+    else
+      out=out..c
+    end
+  end
+  printh(out)
+end
+
+function field_interacter(action, checker, doer)
+  for i = 1, field_size_x do
+    for j = 1, field_size_y do
+      local square = field[i][j]
+      local sx = i*(8+pad_x)
+      local sy = j*(8+pad_y)
+      local dist=max(abs(player.x-sx), abs(player.y-sy))
+      if dist<8 and checker(square) do
+        doer(square)
+        log("[%][%] %", i, j, action)
+      end
+    end
+  end
+end
