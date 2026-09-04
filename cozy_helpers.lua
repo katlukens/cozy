c7e2e44acb35c9b42cc580b0c1f5496fff2c8fba
kat <k.lukens@gmail.com>
2026-09-04 11:18:05 -0600

    moved logic to own file

--- cozy_helpers.lua
+++ cozy_helpers.lua
@@ -1,23 +1,3 @@
-function is_expired(start_time, duration_s)
-  return (start_time+duration_s)<time()
-end
-
-function is_wet(square)
-  return square.spr_soil==2
-end
-
-function is_empty(square)
-  return square.spr_plant==0
-end
-
-function is_planted(square)
-  return (square.spr_plant > 2) and (square.spr_plant < 8)
-end
-
-function is_mature(square)
-  return square.spr_plant == 7
-end
-
 function log(fmt, ...)
   local args={...}
   local n=0
